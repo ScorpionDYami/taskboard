f54de0df7f725d6d51077748d1328aaaafdc84d5
Kevin Espinoza <62354142+ScorpionDYami@users.noreply.github.com>
2025-04-11 00:46:10 -0700

    Establishes a 1:N relationship, an instance of the entity containing this field can have multiple instances of the Work entity associated with it, represented by the works list.

--- src/main/java/mx/itson/taskboard/entities/Student.java
+++ src/main/java/mx/itson/taskboard/entities/Student.java
@@ -4,10 +4,14 @@
  */
 package mx.itson.taskboard.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -15,12 +19,44 @@ import javax.persistence.Id;
  */
 @Entity
 public class Student {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
+    private int idColt;
+    
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Work> works = new ArrayList<>();
+
+    /**
+     * @return the works
+     */
+    public List<Work> getWorks() {
+        return works;
+    }
+
+    /**
+     * @param works the works to set
+     */
+    public void setWorks(List<Work> works) {
+        this.works = works;
+    }
+    
+    /**
+     * @return the idColt
+     */
+    public int getIdColt() {
+        return idColt;
+    }
+
+    /**
+     * @param idColt the idColt to set
+     */
+    public void setIdColt(int idColt) {
+        this.idColt = idColt;
+    }
     
     /**
      * @return the id
